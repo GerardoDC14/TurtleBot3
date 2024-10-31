cab12730b9b35eca8f716a213bf67f00db681c8d
GerardoDC14 <gerardodelcid16@gmail.com>
2024-10-30 23:51:32 -0600

    Fix | Cloned repo

--- build/motor_position_controller/prefix_override/sitecustomize.py
+++ build/motor_position_controller/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/gerardo/turtlebot3_group6v2/install/motor_position_controller'
+    sys.prefix = sys.exec_prefix = '/home/gerardo/turtlebot3_ws/install/motor_position_controller'
--- build/turtlebot3_explore/prefix_override/sitecustomize.py
+++ build/turtlebot3_explore/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/gerardo/turtlebot3_group6v2/install/turtlebot3_explore'
+    sys.prefix = sys.exec_prefix = '/home/gerardo/turtlebot3_ws/install/turtlebot3_explore'
